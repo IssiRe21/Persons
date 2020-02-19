d42577aa4277dcc2760d0ee61f5855af9aaeff14
IssiRe21 <60553960+IssiRe21@users.noreply.github.com>
2020-02-19 11:26:52 -0700

    Persons implemented

---
+++ Person.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Persons
+{
+    class Person
+    {
+        //atributo
+        private string name;
+        static public string bloodColor = "red";
+        public Person(string name) {
+        this.name = name;
+        }
+
+        // Metodo
+        public void SayHello (){
+            Console.WriteLine("Hello, my name is "+ this.name);
+        }
+
+        static public void PrintRace(){
+            Console.WriteLine("Human");
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Persons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Person Jack = new Person("Jack Sparrow");
+           Jack.SayHello();
+
+           Person Pedro = new Person("Pedro Juan");
+           Pedro.SayHello();
+
+          Console.WriteLine(Person.bloodColor);
+
+          Person.PrintRace();
+
+        }
+
+
+    }
+
+}
